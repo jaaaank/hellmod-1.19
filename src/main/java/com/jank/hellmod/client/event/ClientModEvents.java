a154186816b021d6671e5c19f25dfc9ead4ba289
jamnk <pancakes3825@gmail.com>
2022-07-20 23:59:57 -0400

    yeah

--- src/main/java/com/jank/hellmod/client/event/ClientModEvents.java
+++ src/main/java/com/jank/hellmod/client/event/ClientModEvents.java
@@ -1,19 +1,26 @@
 package com.jank.hellmod.client.event;
 
 import com.jank.hellmod.EnhancedHell;
+import com.jank.hellmod.client.armor.DevilArmorRenderer;
 import com.jank.hellmod.client.renderer.BloodDevilGeoRenderer;
 import com.jank.hellmod.client.renderer.BunkerGeoRenderer;
 import com.jank.hellmod.client.renderer.ChaosDevilGeoRenderer;
 import com.jank.hellmod.client.renderer.ReaperGeoRenderer;
+import core.custom.item.DevilArmorItem;
 import core.init.EntityInit;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import software.bernie.geckolib3.renderers.geo.GeoArmorRenderer;
 
 @Mod.EventBusSubscriber(modid = EnhancedHell.MODID, bus = Bus.MOD, value = Dist.CLIENT)
 public final class ClientModEvents {
+	@SubscribeEvent
+	public static void registerArmorRenderers(final EntityRenderersEvent.AddLayers event) {
+		GeoArmorRenderer.registerArmorRenderer(DevilArmorItem.class, new DevilArmorRenderer());
+	}
 
 	private ClientModEvents() {}
 	
